4f9d1f89dc4b14a787d07c303ed2a39fd508a6bf
Bhopal Singh <bhopalsinghsis@gmail.com>
2019-07-03 22:28:20 +0400

    Express Routing Config

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a TEST');
+});
+
+router.get('/cool', function(req, res, next) {
+  res.send('You are so cool!!!!');
 });
 
 module.exports = router;
